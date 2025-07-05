cb54f292f8876c9bb58c443a1310c83d185d062b
Tenzin <tenzindargyal909@gmail.com>
2025-07-05 14:19:51 +0530

    Update script.js

--- java_script/script.js
+++ java_script/script.js
@@ -2,3 +2,18 @@ document.getElementById('contactForm')?.addEventListener('submit', function (e) 
   e.preventDefault();
   alert('Thank you for contacting us!');
 });
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  centeredSlides: true,
+  loop: false, // or true if you want looping
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
