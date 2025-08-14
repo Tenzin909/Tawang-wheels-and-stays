de9361312e12a0712707e25bfa6a9781f2f63b39
Tenzin <tenzindargyal909@gmail.com>
2025-08-15 01:05:16 +0530

    Update script.js

--- java_script/script.js
+++ java_script/script.js
@@ -24,3 +24,23 @@ const swiper = new Swiper(".mySwiper", {
   },
 });
 
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > lastScrollY) {
+    // Scrolling down
+    header.classList.add("hide");
+  } else {
+    // Scrolling up
+    header.classList.remove("hide");
+  }
+
+  lastScrollY = window.scrollY;
+});
+
+
+
+
