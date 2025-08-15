00f77e9fe8e9c3b962809d8ee73e13ec401a5dcc
Tenzin <tenzindargyal909@gmail.com>
2025-08-15 16:45:55 +0530

    Update script.js

--- java_script/script.js
+++ java_script/script.js
@@ -3,28 +3,6 @@ document.getElementById('contactForm')?.addEventListener('submit', function (e) 
   alert('Thank you for contacting us!');
 });
 
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  loop: true,
-  centeredSlides: true, // ✅ This solves the centering issue
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      centeredSlides: false // Disable center mode on wider screens
-    },
-    1024: {
-      slidesPerView: 3,
-      centeredSlides: false
-    },
-  },
-});
-
-
 let lastScroll = 0;
 const header = document.querySelector("header");
 
